18e1e52b97a0afa5b4a434663d1e44659effeb35
LaurenV <laurenvdl45@gmail.com>
2022-11-30 16:05:52 +0100

    feat: signup and login users with mongodb

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // add mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://m001-student:9587kmseja336@sandbox.z5noqyn.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true});
 
 //mongodb://localhost:27017/
 //mongodb+srv://m001-student:9587kmseja336@sandbox.z5noqyn.mongodb.net/?retryWrites=true&w=majority
