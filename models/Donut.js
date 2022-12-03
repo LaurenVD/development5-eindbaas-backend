f1fb485bb13ef8fd41132a4bed00e9b2252c03fe
LaurenV <laurenvdl45@gmail.com>
2022-12-03 15:51:21 +0100

    fix: namechanges

--- models/Donut.js
+++ models/Donut.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const donutSchema = new Schema({
-    username: String,
     email: String,
     sprinkles: String,
     name: String,
     dough: String,
     glaze: String,
     image: String,
-    status: String,
+    production: String
 });
 const Donut = mongoose.model('Donut', donutSchema);
 
